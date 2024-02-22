ed70735688a4e6ea2721975df3657a18ea4ea915
Vitor Freitas <vitor.lucfreitas@gmail.com>
2024-02-22 13:21:47 +0000

    Modifies Selection file

--- sorting/selection-sort/SelectionSort/src/selectionsort/SelectionSort.java
+++ sorting/selection-sort/SelectionSort/src/selectionsort/SelectionSort.java
@@ -1,22 +1,36 @@
 package selectionsort;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class SelectionSort {
     
     public static void main(String[] args) {
         
-        List<Integer> numbers = 
-                new ArrayList(Arrays.asList(15, 50, 45, 1515, 10, 45, 11));
+//        List<Integer> smallList = new ArrayList();
+//        smallList.addAll(addNumbers(100));
+//        recordsTimeComplexity(smallList);
+//        // Nanoseconds: 762100
+//        // Miliseconds: 0.0
+//        
+//        List<Integer> mediumList = new ArrayList();
+//        mediumList.addAll(addNumbers(1_000));
+//        recordsTimeComplexity(mediumList);
+
+        List<Integer> largeList = new ArrayList();
+        largeList.addAll(addNumbers(1_000_000));
+        recordsTimeComplexity(largeList);
+
+        // Do you want to explode your pc?
+//        List<Integer> massiveList = new ArrayList();
+//        massiveList.addAll(addNumbers(1_000_000_000));
+//        recordsTimeComplexity(massiveList);
+//        // Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+
         
         
-        System.out.println(numbers);
         
-        selectionSort(numbers);
-        
-        System.out.println(numbers);
     }
 
     private static void selectionSort(List<Integer> numbers) {
@@ -45,4 +59,37 @@ public class SelectionSort {
         list.set(indexOfMin, temp);
     }
     
+    private static List<Integer> addNumbers (int quantity) {
+        
+        List<Integer> list = new ArrayList();
+        Random random = new Random();
+        
+        for (int i = 0; i < quantity; i++) {
+            list.add(random.nextInt(100));
+        }
+        
+        return list;
+    }
+
+    private static void recordsTimeComplexity(List list) {
+        
+        // Record the start time in nanoseconds
+        long start = System.nanoTime();
+        
+        // Sort the list
+        selectionSort(list);
+        
+        // Record the end time in nanoseconds
+        long end = System.nanoTime();
+        
+        // Converts in miliseconds
+        long nanoseconds =  end - start;
+        double miliseconds = nanoseconds / 1_000_000;
+
+        System.out.println("Nanoseconds: " + nanoseconds);
+        System.out.println("Miliseconds: " + miliseconds);
+    }
+
 }
+
+
